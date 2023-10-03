279aee13b02e85b8a964098391fed39ed1ab8583
athai <arthur.thai479@gmail.com>
2023-10-03 21:41:03 +1100

    refactor code

--- ECommerce/FileManagement/CheckForFileAndDirectory.cs
+++ ECommerce/FileManagement/CheckForFileAndDirectory.cs
@@ -1,16 +1,13 @@
-﻿using ECommerce.Models;
-using Newtonsoft.Json;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ECommerce.Repository
+namespace ECommerce.FileManagement
 {
-    public class ProductFileManager
+    public class CheckForFileAndDirectory
     {
-
         public static void CheckForDirectory()
         {
             //create folder if doesn't exist
@@ -28,13 +25,10 @@ namespace ECommerce.Repository
                 Console.WriteLine($"Directory was successfully created at {Directory.GetCreationTime(path)}");
 
                 //create file in directory
-
-
             }
             catch (Exception e)
             {
                 Console.WriteLine($"The process failed: {e.ToString}"); //give error message 
-
             }
             finally { }
         }
@@ -43,14 +37,12 @@ namespace ECommerce.Repository
         {
             //create a file in directory if it doesn't exist
             string filePath = @"c:\FileStorage\Test.json";
-
             try
             {
                 if (Directory.Exists(filePath)) //id directory exists then return
                 {
                     return;
                 }
-
                 using (FileStream fs = File.Create(filePath)) ; //create the file
             }
             catch (Exception e)
@@ -58,21 +50,6 @@ namespace ECommerce.Repository
                 Console.WriteLine($"The process failed: {e.ToString}");
             }
             finally { }
-        }
-
-        public static void SerializeToJsonFile(List<Product> ListOfProducts)
-        {
-            string json = $"{JsonConvert.SerializeObject(ListOfProducts, Formatting.Indented)}";
-            File.WriteAllText(@"C:\FileStorage\Test.json", json); //add ListOfProducts <List> into JSON file
-        }
-
-        public static List<Product> DeserializeJsonFileToList()
-        {
-            List<Product> ListOfProducts;
-            //turn the Json file into ListOfProducts so that memory is stored
-            string storedJsonMemory = File.ReadAllText(@"C:\FileStorage\Test.json");
-            ListOfProducts = JsonConvert.DeserializeObject<List<Product>>(storedJsonMemory)!;
-            return ListOfProducts;
         }
     }
 }
---
+++ ECommerce/FileManagement/ProductFileManager.cs
@@ -0,0 +1,28 @@
+﻿using ECommerce.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Repository
+{
+    public class ProductFileManager
+    {
+        public static void SerializeToJsonFile(List<Product> ListOfProducts)
+        {
+            string json = $"{JsonConvert.SerializeObject(ListOfProducts, Formatting.Indented)}";
+            File.WriteAllText(@"C:\FileStorage\Test.json", json); //add ListOfProducts <List> into JSON file
+        }
+
+        public static List<Product> DeserializeJsonFileToList()
+        {
+            List<Product> ListOfProducts;
+            //turn the Json file into ListOfProducts so that memory is stored
+            string storedJsonMemory = File.ReadAllText(@"C:\FileStorage\Test.json");
+            ListOfProducts = JsonConvert.DeserializeObject<List<Product>>(storedJsonMemory)!;
+            return ListOfProducts;
+        }
+    }
+}
