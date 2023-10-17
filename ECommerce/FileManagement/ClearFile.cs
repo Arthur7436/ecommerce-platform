41357cc370619919bd89cc2ebe2227c9e440731b
athai <arthur.thai479@gmail.com>
2023-10-17 11:49:28 +1100

    refactor code

---
+++ ECommerce/FileManagement/ClearFile.cs
@@ -0,0 +1,20 @@
+﻿using ECommerce.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.FileManagement
+{
+    internal class ClearFile
+    {
+        public static void ClearProductList(List<Product> ListOfProducts)
+        {
+            //push those changes and serialize as Json 
+            string json = JsonConvert.SerializeObject(ListOfProducts);
+            File.WriteAllText(@"C:\FileStorage\Test.json", json);
+        }
+    }
+}
