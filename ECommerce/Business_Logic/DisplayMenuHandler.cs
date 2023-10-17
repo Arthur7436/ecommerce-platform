4ee76c2e42aa26d0cdfba0097467d34cecc760b6
athai <arthur.thai479@gmail.com>
2023-10-17 11:44:19 +1100

    refactor code

---
+++ ECommerce/Business_Logic/DisplayMenuHandler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Business_Logic
+{
+    public class DisplayMenuHandler
+    {
+        public static void DisplayMenu()
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Welcome to the E-commerce platform!");
+            Console.ResetColor();
+            Console.WriteLine("Please select one of the following: ");
+
+            List<string> displayMenu = new List<string>()
+                {
+                    "1. View all products",
+                    "2. Add a product",
+                    "3. Remove a product",
+                    "4. Update a product",
+                    "Enter 'r' to reset the list to null",
+                    "Enter 'q' to exit the program"
+                };
+
+
+            for (int i = 0; i < displayMenu.Count; i++)
+            {
+                Console.WriteLine(displayMenu[i]); //display menu
+            }
+        }
+    }
+}
