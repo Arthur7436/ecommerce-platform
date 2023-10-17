527e45def8d7953e4c8bbc57eae49d4ebf3353f8
athai <arthur.thai479@gmail.com>
2023-10-17 11:42:53 +1100

    refactor code

---
+++ ECommerce/Business_Logic/DeleteHandler.cs
@@ -0,0 +1,79 @@
+﻿using ECommerce.Models;
+using ECommerce.Repository;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace ECommerce.Business_Logic
+{
+    public class DeleteHandler : ProductRepository
+    {
+        public static void RemoveProduct(List<Product> ListOfProducts)
+        {
+            ProductDataBaseHandler.SetSqlVariables(out adapter, out sql, out cnn);
+            cnn.Open();
+
+            //collect info from user
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Which product would you like to delete?");
+            Console.ResetColor();
+
+            foreach (Product products in ListOfProducts!) //print out all the products available
+            {
+                Console.WriteLine(products.ToString());
+            }
+
+            ProductDataBaseHandler.ViewSqlDb();
+
+            Console.Write("Input: ");
+            string userRemovalInput = Console.ReadLine()!;
+
+            //remove based on name of product
+            if (!ListOfProducts.Any(x => x.NameOfProduct == userRemovalInput)) //if users inputs a product that already exists => give error
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("The product doesn't exist!");
+                Console.ResetColor();
+
+                Console.ReadLine();
+
+                ProductDataBaseHandler.CloseSqlConnection();
+                return;
+            }
+
+            if (ListOfProducts.Any(x => x.NameOfProduct == userRemovalInput)) //else remove the product from the list
+            {
+                //loop through the whole list 
+                for (int i = 0; i < ListOfProducts.Count; i++)
+                {
+                    //if the name of product is equal to the userRemovalInput, find the index of that object
+                    if (ListOfProducts[i].NameOfProduct == userRemovalInput)
+                    {
+                        //remove from list
+                        ListOfProducts.RemoveAt(i);
+
+                        //remove product also from sql db
+                        sql = $"Delete dbo.Product where Identify={i + 1}";
+
+                        command = new SqlCommand(sql, cnn);
+                        //cnn.Open();
+                        adapter.DeleteCommand = new SqlCommand(sql, cnn);
+                        adapter.DeleteCommand.ExecuteNonQuery();
+
+                        ProductDataBaseHandler.CloseSqlConnection();
+
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Product successfully removed!");
+                        Console.ResetColor();
+
+                        Console.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
