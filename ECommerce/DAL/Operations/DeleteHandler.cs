f7339a0a4eceeb6922ab1f145b2b7592838dba43
athai <arthur.thai479@gmail.com>
2023-10-22 21:42:20 +1100

    refactor code

--- ECommerce/DAL/Operations/DeleteHandler.cs
+++ ECommerce/DAL/Operations/DeleteHandler.cs
@@ -4,6 +4,7 @@ using ECommerce.GlobalVariables;
 using ECommerce.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -35,54 +36,24 @@ namespace ECommerce.DAL.Operations
                 for (int i = 0; i < dataReader.FieldCount; i++)
                 {
                     listOfOutputs.Add(dataReader[i].ToString()!);
-                    string dbName = dataReader[i].ToString()!;
+                    //string dbName = dataReader[i].ToString()!;
 
-                    //if any of the sql names do not match the names within the list then remove it
-                    if (!ListOfProducts[i].NameOfProduct.Equals(dbName))
+
+                    foreach (string dbName in listOfOutputs)
                     {
-                        dataReader.Close();
-                        //use sql command to delete the product
-                        sql = $"Delete from dbo.Product where NameOfProduct='{dbName}'";
+                        if (!ListOfProducts.Any(x => x.NameOfProduct == dbName))
+                        {
+                            sql = $"Delete from dbo.Product where NameOfProduct='{dbName}'";
+                            adapter.DeleteCommand = new SqlCommand(sql, cnn);
+                            adapter.DeleteCommand.ExecuteNonQuery();
 
-                        adapter.DeleteCommand = new SqlCommand(sql, cnn);
-                        adapter.DeleteCommand.ExecuteNonQuery();
+                            CloseSqlConnection.CloseSql();
+                            return;
 
-                        CloseSqlConnection.CloseSql();
-                        return;
+                        }
                     }
                 }
             }
-
-         
-
-
-
-
-
-            //go through the entire list of products
-            //for (int i = 0; i < ListOfProducts.Count; i++)
-            //{
-
-            //    if (ListOfProducts[i] == )
-            //    {
-
-            //    }
-            //}
-
-            ////compare it with sql db
-
-            ////the one that stands out is to be removed
-
-            ////remove product also from sql db
-            //sql = $"Delete dbo.Product where Identify={i + 1}";
-
-            //command = new SqlCommand(sql, cnn);
-            ////cnn.Open();
-            //adapter.DeleteCommand = new SqlCommand(sql, cnn);
-            //adapter.DeleteCommand.ExecuteNonQuery();
-
-            //CloseSqlConnection.CloseSql();
-
         }
     }
 }
