ba931ef091050a8ade49e5f73f3fe4996636b38d
athai <arthur.thai479@gmail.com>
2023-11-10 09:18:59 +1100

    update

--- ECommerce/Program.cs
+++ ECommerce/Program.cs
@@ -15,7 +15,7 @@ namespace ECommercePlatform
         static void Main(string[] args)
         {
             List<Product> ListOfProducts = new List<Product>(); //create a list to store all products inside
-            ConnectToSqlDB.ConnectToSqlDb(); //connect program to databaseeeee
+            ConnectToSqlDB.ConnectToSqlDb(); //connect program to databaseeeeee
             CheckForFileAndDirectory.CheckForDirectory();
             CheckForFileAndDirectory.CheckForFile(); 
 
