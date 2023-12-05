835264bde211ff549cfefbd9f1f2dca11d0d074d
athai <arthur.thai479@gmail.com>
2023-12-05 21:59:35 +1100

    refactor code

--- ECommerce/Models/Order.cs
+++ ECommerce/Models/Order.cs
@@ -6,10 +6,11 @@ using System.Threading.Tasks;
 
 namespace ECommerce.Models
 {
+    //basic order class where products can be associated to
     public class Order
     {
         public string OrderId { get; set; }
         public DateTime OrderDate { get; set;  }
-        public List<Product> Products { get; set;  } = new List<Product>();
+        public List<Product> Products { get; set;  } = new List<Product>(); //initialises Products list to make sure it is not null
     }
 }
