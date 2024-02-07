66a81635b5232dd2ba54c8a089efe8b1c7ec2348
arthur <arthur.thai479@gmail.com>
2024-02-07 21:19:00 +1100

    update

--- ECommerce/DAL/DataBaseHandler.cs
+++ ECommerce/DAL/DataBaseHandler.cs
@@ -16,9 +16,9 @@ namespace ECommerce.Repository
         public static string? sql;
         public static SqlConnection? cnn;
         public static string usernameSecret = File.ReadAllText(@"C:\ProjectSecrets\username.txt");
-        public static string username = usernameSecret;
+        public static string username = usernameSecret; //store the file fath of username.txt in a variable
         public static string dataSourceName = File.ReadAllText(@"C:\ProjectSecrets\dataSourceName.txt");
-        public static string dataSource = dataSourceName;
+        public static string dataSource = dataSourceName; //store the file fath of dataSourceName.txt in a variable
         public static string dataBase = "dbProduct";
         public static string path = @"C:\ProjectSecrets\EcommerceSecrets.txt";
         public static string pwd = File.ReadAllText(path);
